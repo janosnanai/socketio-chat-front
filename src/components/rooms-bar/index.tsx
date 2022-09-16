08c7b43471e7c07983ee4b7183973a442e84892b
Janos Nanai <janos.nanai@gmail.com>
2022-09-16 23:01:45 +0200

    rooms-bar styling

--- src/components/rooms-bar/index.tsx
+++ src/components/rooms-bar/index.tsx
@@ -2,6 +2,7 @@ import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { Disclosure, RadioGroup } from "@headlessui/react";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { UsersIcon, UserCircleIcon } from "@heroicons/react/20/solid";
 
 import {
   currentRoomGetterAtom,
@@ -28,40 +29,77 @@ function RoomsBar({
   }, [rooms, users]);
 
   return (
-    <aside>
+    <aside className="p-3 rounded-lg bg-zinc-900">
       <RadioGroup value={currentRoom} onChange={changeHandler}>
-        {roomsWithUsers &&
-          rooms.map((room) => (
-            <div className="flex gap-3" key={"r" + room.id}>
-              <RadioGroup.Option value={room}>
-                {({ checked }) => (
-                  <span
-                    className={`inline-block h-6 w-6 rounded-full ${
-                      checked ? "bg-emerald-500" : "bg-neutral-500"
-                    }`}
-                  />
-                )}
-              </RadioGroup.Option>
-              <Disclosure>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button>
-                      <span>{room.name}</span>
-                      <ChevronRightIcon className="h-6 w-6" />
-                    </Disclosure.Button>
-                    <Disclosure.Panel>
-                      <ul>
-                        {roomsWithUsers[room.id] &&
-                          roomsWithUsers[room.id].map((user) => (
-                            <li key={user.id}>{user.username}</li>
-                          ))}
-                      </ul>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            </div>
-          ))}
+        <div className="space-y-2">
+          {roomsWithUsers &&
+            rooms.map((room) => (
+              <div className="flex gap-3" key={"r" + room.id}>
+                <RadioGroup.Option value={room} className="h-min">
+                  {({ checked }) => (
+                    <span
+                      className={`cursor-pointer inline-block mt-3 h-5 w-5 rounded-full transition-colors duration-300 ease-out hover:border-spacing-2 ${
+                        checked
+                          ? "bg-emerald-500"
+                          : "bg-neutral-600 hover:bg-neutral-500"
+                      }`}
+                    />
+                  )}
+                </RadioGroup.Option>
+                <div>
+                  <Disclosure defaultOpen>
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button
+                          className={`p-2 rounded-lg transition-colors duration-300 ${
+                            currentRoom?.id === room.id
+                              ? "text-emerald-300 bg-zinc-700"
+                              : "text-zinc-400 hover:text-zinc-200 bg-zinc-800"
+                          }`}
+                        >
+                          <div className="flex justify-between items-center w-44">
+                            <div>
+                              <h3 className="inline-block leading-none truncate">
+                                {room.name}
+                              </h3>
+                              <div className="flex gap-0.5 items-center">
+                                <UsersIcon className="h-2.5 w-2.5" />
+                                <span className="inline-block text-xs font-mono">
+                                  {Array.isArray(roomsWithUsers[room.id]) &&
+                                    roomsWithUsers[room.id].length}
+                                </span>
+                              </div>
+                            </div>
+                            <ChevronRightIcon
+                              className={`h-5 w-5 transition-transform ${
+                                open ? "rotate-90" : "rotate-0"
+                              }`}
+                            />
+                          </div>
+                        </Disclosure.Button>
+                        <Disclosure.Panel as="ul">
+                          {roomsWithUsers[room.id] &&
+                            roomsWithUsers[room.id].map((user) => (
+                              <li
+                                key={user.id}
+                                className="text-zinc-500 flex gap-1 items-center"
+                              >
+                                <span className="inline-block">
+                                  <UserCircleIcon className="h-3 w-3" />
+                                </span>
+                                <span className="inline-block">
+                                  {user.username}
+                                </span>
+                              </li>
+                            ))}
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                </div>
+              </div>
+            ))}
+        </div>
       </RadioGroup>
     </aside>
   );
