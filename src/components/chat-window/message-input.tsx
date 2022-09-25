724a7d20c74825ec5a181570d1eebb5451ae6813
Janos Nanai <janos.nanai@gmail.com>
2022-09-25 07:39:58 +0200

    feat: chat window disabled state

--- src/components/chat-window/message-input.tsx
+++ src/components/chat-window/message-input.tsx
@@ -4,11 +4,16 @@ import { useAtom } from "jotai";
 import { useState } from "react";
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 
-import { socketAtom, thisUserGetterAtom } from "../../lib/atoms";
+import {
+  currentRoomGetterAtom,
+  socketAtom,
+  thisUserGetterAtom,
+} from "../../lib/atoms";
 import { EventTypes } from "../../lib/constants";
 
-function MessageInput() {
+function MessageInput({ disabled = false }: { disabled?: boolean }) {
   const [messageInput, setMessageInput] = useState("");
+  const [currentRoom] = useAtom(currentRoomGetterAtom);
   const [socket] = useAtom(socketAtom);
   const [user] = useAtom(thisUserGetterAtom);
 
@@ -57,13 +62,15 @@ function MessageInput() {
             value={messageInput}
             id="message"
             rows={2}
-            placeholder="type a message..."
+            placeholder={disabled || !currentRoom ? "" : "type a message..."}
             className="px-2 py-1 w-full resize-none overflow-y-auto text-zinc-900 dark:text-zinc-100 bg-zinc-200 dark:bg-zinc-900 placeholder:text-zinc-500 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-black/25 hover:scrollbar-thumb-black/30 dark:scrollbar-thumb-white/25 dark:hover:scrollbar-thumb-white/30"
+            disabled={disabled || !currentRoom}
           />
         </div>
         <button
           type="submit"
-          className="p-2 h-11 w-11 my-auto mx-2 transition bg-emerald-500 hover:bg-emerald-400 rounded-full shadow-lg hover:shadow-xl"
+          className="p-2 h-11 w-11 my-auto mx-2 transition bg-emerald-500 hover:enabled:bg-emerald-400 rounded-full shadow-lg hover:enabled:shadow-xl disabled:bg-zinc-500"
+          disabled={disabled || !currentRoom}
         >
           <PaperAirplaneIcon className="ml-0.5 h-6 w-6" />
         </button>
--- src/components/chat-window/room-chat-window.tsx
+++ src/components/chat-window/room-chat-window.tsx
@@ -67,64 +67,64 @@ function RoomChatWindow() {
 
   return (
     <div className="flex flex-col gap-2 h-96 w-full">
-      {room && (
-        <>
-          <div className="flex justify-between h-9 pl-3 pr-1 py-1 bg-zinc-100/50 dark:bg-zinc-900/50 rounded-lg shadow-lg">
-            <h1 className="text-xl text-zinc-900 dark:text-zinc-300">
-              {room.name}
-            </h1>
-            {socket.connected && (
-              <button
-                onClick={leaveRoomhandler}
-                className="px-2 transition-colors text-zinc-700 dark:text-zinc-500 hover:text-zinc-800 dark:hover:text-zinc-300 bg-transparent hover:bg-red-500/60 uppercase rounded-lg"
-              >
-                leave
-              </button>
-            )}
+      <div className="flex justify-between h-9 pl-3 pr-1 py-1 bg-zinc-100/50 dark:bg-zinc-900/50 rounded-lg shadow-lg">
+        <h1
+          className={`text-xl ${
+            room ? "text-zinc-900 dark:text-zinc-300" : "text-zinc-500"
+          }`}
+        >
+          {room ? room.name : "--no room selected--"}
+        </h1>
+        {socket.connected && room && (
+          <button
+            onClick={leaveRoomhandler}
+            className="px-2 transition-colors text-zinc-700 dark:text-zinc-500 hover:text-zinc-800 dark:hover:text-zinc-300 bg-transparent hover:bg-red-500/60 uppercase rounded-lg"
+          >
+            leave
+          </button>
+        )}
+      </div>
+      <div className="flex-grow bg-zinc-200 dark:bg-zinc-900 rounded-lg shadow-lg overflow-hidden">
+        <div
+          id="chat-window"
+          className="h-full w-full pr-1 overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-black/25 hover:scrollbar-thumb-black/30 dark:scrollbar-thumb-white/25 dark:hover:scrollbar-thumb-white/30"
+        >
+          {messages && (
+            <ul>
+              {messages.map((message) => {
+                if (message.type === MessageTypes.SERVER) {
+                  const serverMessage = message as ServerMsg;
+                  return (
+                    <li key={serverMessage.id}>
+                      <ServerMessageBubble message={serverMessage} />
+                    </li>
+                  );
+                }
+                if (message.type === MessageTypes.CLIENT) {
+                  const clientMessage = message as ClientMsg;
+                  return (
+                    <li key={message.id}>
+                      <ClientMessageBubble
+                        message={clientMessage}
+                        isOwn={user.id === clientMessage.author.id}
+                      />
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          )}
+          <div className="h-6 mb-3 mr-5">
+            <SyncLoader
+              loading={showTyping}
+              className="text-right"
+              color="#9936d6"
+              size={5}
+              speedMultiplier={0.8}
+            />
           </div>
-          <div className="flex-grow bg-zinc-200 dark:bg-zinc-900 rounded-lg shadow-lg overflow-hidden">
-            <div
-              id="chat-window"
-              className="h-full w-full pr-1 overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-black/25 hover:scrollbar-thumb-black/30 dark:scrollbar-thumb-white/25 dark:hover:scrollbar-thumb-white/30"
-            >
-              {messages && (
-                <ul>
-                  {messages.map((message) => {
-                    if (message.type === MessageTypes.SERVER) {
-                      const serverMessage = message as ServerMsg;
-                      return (
-                        <li key={serverMessage.id}>
-                          <ServerMessageBubble message={serverMessage} />
-                        </li>
-                      );
-                    }
-                    if (message.type === MessageTypes.CLIENT) {
-                      const clientMessage = message as ClientMsg;
-                      return (
-                        <li key={message.id}>
-                          <ClientMessageBubble
-                            message={clientMessage}
-                            isOwn={user.id === clientMessage.author.id}
-                          />
-                        </li>
-                      );
-                    }
-                  })}
-                </ul>
-              )}
-              <div className="h-6 mb-3 mr-5">
-                <SyncLoader
-                  loading={showTyping}
-                  className="text-right"
-                  color="#9936d6"
-                  size={5}
-                  speedMultiplier={0.8}
-                />
-              </div>
-            </div>
-          </div>
-        </>
-      )}
+        </div>
+      </div>
     </div>
   );
 }
